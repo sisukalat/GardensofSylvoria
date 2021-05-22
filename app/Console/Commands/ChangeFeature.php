3d4f6e8298d8d869b96768a8c9d2f751e352af22
Newt <dragonminer101@gmail.com>
2021-05-22 20:25:32 +0100

    stashing

---
+++ app/Console/Commands/ChangeFeature.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Settings;
+use Log;
+use Illuminate\Console\Command;
+use App\Models\Character\Character;
+
+class ChangeFeature extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'change-feature';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Changes current featured character.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $characterCount = Character::count();
+        $setting = Settings::get('featured_character');
+        //
+        Log::debug($setting);
+        if($characterCount && $setting) {
+            $id = mt_rand(1, $characterCount);
+            //
+            if($id == $setting->value) {
+                $id = $id + mt_rand(-5, 5);
+            }
+
+        }
+    }
+}
