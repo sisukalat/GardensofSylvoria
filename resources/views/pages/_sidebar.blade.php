81a0d35ea27ee85edfcf30e607c256ae503667da
Newt <dragonminer101@gmail.com>
2021-05-23 20:41:45 +0100

    c'est fini en ce moment

--- resources/views/pages/_sidebar.blade.php
+++ resources/views/pages/_sidebar.blade.php
@@ -1,5 +1,5 @@
 <ul class="text-center">
-    <li class="sidebar-header"><a href="{{ url('shops') }}" class="card-link">Featured Character</a></li>
+    <li class="sidebar-header"><a href="#" class="card-link">Featured Character</a></li>
 
     <li class="sidebar-section p-2">
         @if(isset($featured) && $featured)
