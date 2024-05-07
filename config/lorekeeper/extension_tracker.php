5bcde0533eedebd6bddc474107639d983e955fd7
jam <ch3rvbcreations@gmail.com>
2024-05-07 08:11:47 -0400

    :bug: oops lost sorry you aren't in this episode

--- config/lorekeeper/extension_tracker.php
+++ config/lorekeeper/extension_tracker.php
@@ -17,7 +17,6 @@ return [
         'wiki_key' => 'Higher_or_Lower',
         'creators' => json_encode([
             'CH3RVB' => 'https://github.com/CH3RVB/',
-            'LostInProgres' => 'https://github.com/LostInProgres',
         ]),
         'version' => '1.0.1',
     ],
