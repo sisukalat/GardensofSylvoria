35ea06b54408ff1c9e1e92e5ea53305121e12e34
LAPTOP-BM077HMN\amalw <sisukalat@gmail.com>
2025-01-30 01:20:52 -0500

    testing?

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -10,5 +10,5 @@
         <li class="nav-item"><a href="{{ url('credits') }}" class="nav-link">Credits</a></li>
     </ul>
 </nav>
-<i class="far fa-clock"></i> {!! LiveClock("America/New_York") !!}
+<div><i class="far fa-clock"></i> {!! LiveClock("America/New_York") !!}</div>
 <div class="copyright">&copy; {{ config('lorekeeper.settings.site_name', 'Lorekeeper') }} v{{ config('lorekeeper.settings.version') }} {{ Carbon\Carbon::now()->year }}</div>
